0fac4a8ca40a6c07f056547932d91a9d04b6f926
CreeperLordVader-Iclucia-Ashina <1749065611@qq.com>
2022-05-06 16:26:03 +0800

    Implement the shader for sword

--- shaders/Sword.js
+++ shaders/Sword.js
@@ -1,5 +1,21 @@
-const SwordFragmentShader=
-''
-
 const SwordVertexShader=
-''
+`attribute vec3 vPos;
+attribute vec3 vColor;
+uniform mat4 view;
+uniform mat4 proj;
+uniform mat4 model;
+varying highp vec4 aColor;
+void main()
+{
+    gl_Position=proj*view*model*vec4(vPos.x,vPos.y,vPos.z,1.0);
+    aColor=vec4(vColor.x,vColor.y,vColor.z,1.0);
+}
+`
+
+const SwordFragmentShader=
+`varying vec4 aColor;
+void main()
+{
+    gl_FragColor=aColor;
+}
+`;
